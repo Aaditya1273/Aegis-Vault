6b608f324b98c5784964178116975bdeb3e714a6
IOCRYPOSTO <aadityaiitian1515@gmail.com>
2026-03-20 18:46:28 +0530

    Delete sdk directory

--- sdk/index.ts
+++
@@ -1,7 +0,0 @@
-// LUMINAUT SDK - Official TypeScript SDK for LUMINAUT Wallet API
-export * from "./client"
-export * from "./types"
-export * from "./wallet"
-export * from "./token"
-export * from "./ai"
-export * from "./social"
